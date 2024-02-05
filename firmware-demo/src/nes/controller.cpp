8b9157b3b289b3b6db3a7dec1bacf16228ff421a
Andrew Dunai <a@dun.ai>
2024-02-05 17:34:16 +0200

    nofrendo: fix bug in controller read

--- firmware-demo/src/nes/controller.cpp
+++ firmware-demo/src/nes/controller.cpp
@@ -10,5 +10,5 @@ extern "C" void controller_init() {}
 
 extern "C" uint32_t controller_read_input() {
     lilka::input_t input = lilka::controller.read();
-    return 0xFFFFFFFF ^ (input.up << 0) | (input.down << 1) | (input.left << 2) | (input.right << 3) | (input.select << 4) | (input.start << 5) | (input.a << 6) | (input.b << 7);
+    return 0xFFFFFFFF ^ ((input.up << 0) | (input.down << 1) | (input.left << 2) | (input.right << 3) | (input.select << 4) | (input.start << 5) | (input.a << 6) | (input.b << 7));
 }
