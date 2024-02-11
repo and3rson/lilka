7ea7b1c581bec82475b55d40023a594abc25872d
Andrew Dunai <a@dun.ai>
2024-02-11 17:24:28 +0200

    doom: PoC of DOOM on Lilka v2

--- firmware-doom/src/main.cpp
+++ firmware-doom/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "lilka.h"
 
 extern "C" {
 #include "doomkeys.h"
@@ -12,9 +13,13 @@ extern void doomgeneric_Tick();
 char foo[1024];
 
 void setup() {
-    int argc = 1;
+    lilka::begin();
+
+    int argc = 3;
     char arg[] = "doomgeneric";
-    char *argv[] = {arg};
+    char arg2[] = "-iwad";
+    char arg3[] = "/sd/doom.wad";
+    char *argv[] = {arg, arg2, arg3};
 
     Serial.begin(115200);
     Serial.println("Doomgeneric starting...");
@@ -31,10 +36,46 @@ void setup() {
     }
 }
 
-extern "C" void DG_Init() {
-}
+extern "C" void DG_Init() {}
 
 extern "C" void DG_DrawFrame() {
+    Serial.println("Draw frame");
+    lilka::display.startWrite();
+    lilka::display.writeAddrWindow(0, 65, 240, 150);
+    uint16_t row[240];
+    // Convert 640x400 to 240x150
+    for (int y = 0; y < 150; y++) {
+        for (int x = 0; x < 240; x++) {
+            int yy = y * 8 / 3;
+            int xx = x * 8 / 3;
+            uint32_t pixel = DG_ScreenBuffer[yy * 640 + xx];
+            uint8_t r = (pixel >> 16) & 0xff;
+            uint8_t g = (pixel >> 8) & 0xff;
+            uint8_t b = pixel & 0xff;
+            row[x] = lilka::display.color565(r, g, b);
+        }
+        lilka::display.writePixels(row, 240);
+    }
+    // for (int y = 0; y < 200; y++) {
+    //     // For every 8 rows, draw only 3
+    //     if (y % 8 != 0 && y % 8 != 3 && y % 8 != 6) {
+    //         continue;
+    //     }
+    //     uint8_t pixelIndex = 0;
+    //     for (int x = 0; x < 320; x++) {
+    //         // For every 8 pixels, draw only 3
+    //         if (x % 8 != 0 && x % 8 != 3 && x % 8 != 6) {
+    //             continue;
+    //         }
+    //         uint32_t pixel = DG_ScreenBuffer[y * 320 + x];
+    //         uint8_t r = (pixel >> 16) & 0xff;
+    //         uint8_t g = (pixel >> 8) & 0xff;
+    //         uint8_t b = pixel & 0xff;
+    //         row[pixelIndex++] = lilka::display.color565(r, g, b);
+    //     }
+    //     lilka::display.writePixels(row, 240);
+    // }
+    lilka::display.endWrite();
 }
 
 extern "C" void DG_SetWindowTitle(const char *title) {
