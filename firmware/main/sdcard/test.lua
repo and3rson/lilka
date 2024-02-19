d0ad855a4a24d9b3e311adaf8ff597563a367ed0
Andrew Dunai <a@dun.ai>
2024-02-20 00:49:21 +0200

    main: add image loading & drawing to Lua, update Lua demo, fix console logs

--- firmware/main/sdcard/test.lua
+++ firmware/main/sdcard/test.lua
@@ -1,29 +1,41 @@
 local display = require("display")
-local console = require("console")
 local controller = require("controller")
 local util = require("util")
+local resources = require("resources")
 
-console.print("Printing stuff to console, yay!")
+print("Printing stuff to console, yay!")
+
+local face = resources.load_image("face.bmp")
 
 for i = 10, 1, -1 do
     display.fill_rect(0, 0, display.width, display.height, util.random(0xFFFF))
     display.set_cursor(64, 64)
     display.print("Start in " .. i .. "...")
+
+    x = util.random(240 - 64)
+    y = util.random(280 - 64)
+    display.draw_image(face, x, y, display.color565(0, 0, 0))
+
     display.render()
+
     util.delay(250)
 end
 
--- Now, we draw lines really fast directly to display!
+-- Now, we want to draw random lines & faces really fast directly to display, without any buffering!
 display.set_buffered(false)
 
 local key = controller.get_state()
--- Loop while key.a.just_pressed is false
 while not key.a.just_pressed do
-    key = controller.get_state()
     local x1 = util.random(240)
     local y1 = util.random(280)
     local x2 = util.random(240)
     local y2 = util.random(280)
     local color = util.random(0xFFFF)
     display.draw_line(x1, y1, x2, y2, color)
+
+    local x = util.random(240 - 64)
+    local y = util.random(280 - 64)
+    display.draw_image(face, x, y, display.color565(0, 0, 0))
+
+    key = controller.get_state()
 end
--- firmware/main/src/lua_runner.cpp
+++ firmware/main/src/lua_runner.cpp
@@ -30,6 +30,15 @@ int lualilka_display_setBuffered(lua_State* L) {
     return 0;
 }
 
+int lualilka_display_color565(lua_State* L) {
+    int r = luaL_checkinteger(L, 1);
+    int g = luaL_checkinteger(L, 2);
+    int b = luaL_checkinteger(L, 3);
+    uint16_t color = lilka::display.color565(r, g, b);
+    lua_pushinteger(L, color);
+    return 1;
+}
+
 int lualilka_display_setCursor(lua_State* L) {
     int x = luaL_checkinteger(L, 1);
     int y = luaL_checkinteger(L, 2);
@@ -44,6 +53,8 @@ int lualilka_display_print(lua_State* L) {
             getDrawable(L)->print(lua_tostring(L, i));
         } else if (lua_isnumber(L, i)) {
             getDrawable(L)->print(lua_tonumber(L, i));
+        } else if (lua_islightuserdata(L, i)) {
+            getDrawable(L)->printf("%p", lua_topointer(L, i));
         } else {
             getDrawable(L)->print(lua_typename(L, lua_type(L, i)));
         }
@@ -70,6 +81,31 @@ int lualilka_display_fillRect(lua_State* L) {
     getDrawable(L)->fillRect(x, y, w, h, color);
     return 0;
 }
+
+int lualilka_display_drawImage(lua_State* L) {
+    // Args are image handler, X & Y
+    // First argument is pointer to image data
+    uint16_t* pixels = (uint16_t*)lua_touserdata(L, 1);
+    int16_t x = luaL_checkinteger(L, 2);
+    int16_t y = luaL_checkinteger(L, 3);
+    // Fourth, optional argument is transparent color
+    int32_t transparent = -1;
+    if (lua_gettop(L) == 4) {
+        transparent = luaL_checkinteger(L, 4);
+    }
+
+    int16_t width = pixels[0];
+    int16_t height = pixels[1];
+    pixels += 2;
+
+    if (transparent >= 0) {
+        getDrawable(L)->draw16bitRGBBitmapWithTranColor(x, y, pixels, transparent, width, height);
+    } else {
+        getDrawable(L)->draw16bitRGBBitmap(x, y, pixels, width, height);
+    }
+
+    return 0;
+};
 
 int lualilka_display_render(lua_State* L) {
     // Check if display is buffered
@@ -98,7 +134,7 @@ int lualilka_display_render(lua_State* L) {
 // }
 
 static const luaL_Reg lualilka_display[] = {
-    {"set_buffered", lualilka_display_setBuffered}, {"set_cursor", lualilka_display_setCursor}, {"print", lualilka_display_print}, {"draw_line", lualilka_display_drawLine}, {"fill_rect", lualilka_display_fillRect}, {"render", lualilka_display_render}, {NULL, NULL},
+    {"set_buffered", lualilka_display_setBuffered}, {"color565", lualilka_display_color565}, {"set_cursor", lualilka_display_setCursor}, {"print", lualilka_display_print}, {"draw_line", lualilka_display_drawLine}, {"fill_rect", lualilka_display_fillRect}, {"draw_image", lualilka_display_drawImage}, {"render", lualilka_display_render}, {NULL, NULL},
 };
 
 int luaopen_lilka_display(lua_State* L) {
@@ -119,28 +155,24 @@ int lualilka_console_print(lua_State* L) {
     int n = lua_gettop(L);
     for (int i = 1; i <= n; i++) {
         if (lua_isstring(L, i)) {
-            Serial.printf("%s", lua_tostring(L, i));
+            printf("%s", lua_tostring(L, i));
         } else if (lua_isnumber(L, i)) {
-            Serial.printf("%g", lua_tonumber(L, i));
+            printf("%g", lua_tonumber(L, i));
+        } else if (lua_islightuserdata(L, i)) {
+            printf("%p", lua_topointer(L, i));
         } else {
-            Serial.printf("%s", lua_typename(L, lua_type(L, i)));
+            printf("%s", lua_typename(L, lua_type(L, i)));
         }
         if (i < n) {
-            Serial.printf("\t");
+            printf("\t");
         }
     }
-    return 0;
-}
-
-int lualilka_console_println(lua_State* L) {
-    lualilka_console_print(L);
-    Serial.printf("\n");
+    printf("\n");
     return 0;
 }
 
 static const luaL_Reg lualilka_console[] = {
     {"print", lualilka_console_print},
-    {"println", lualilka_console_println},
     {NULL, NULL},
 };
 
@@ -227,39 +259,124 @@ int luaopen_lilka_util(lua_State* L) {
     return 1;
 }
 
+int lualilka_resources_loadImage(lua_State* L) {
+    const char* path = luaL_checkstring(L, 1);
+    // Get dir from registry
+    lua_getfield(L, LUA_REGISTRYINDEX, "dir");
+    const char* dir = lua_tostring(L, -1);
+    lua_pop(L, 1);
+    String fullPath = String(dir) + "/" + path;
+    FILE* file = fopen(fullPath.c_str(), "r");
+    if (!file) {
+        return luaL_error(L, "File not found - %s", fullPath.c_str());
+    }
+    // Parse BMP image
+    uint8_t fileheader[14];
+    fread(fileheader, 1, 14, file);
+    int dataOffset = fileheader[10] + (fileheader[11] << 8) + (fileheader[12] << 16) + (fileheader[13] << 24);
+    uint8_t fileinfo[40];
+    fread(fileinfo, 1, 40, file);
+    int width = fileinfo[4] + (fileinfo[5] << 8) + (fileinfo[6] << 16) + (fileinfo[7] << 24);
+    int height = fileinfo[8] + (fileinfo[9] << 8) + (fileinfo[10] << 16) + (fileinfo[11] << 24);
+    // int bitsPerPixel = fileinfo[14] + (fileinfo[15] << 8);
+    // uint16_t *pixels = new uint16_t[width * height];
+
+    // First two elements are width and height
+    uint16_t* pixels = new uint16_t[2 + width * height];
+    pixels[0] = width;
+    pixels[1] = height;
+    lilka::serial_log("Lua: loading image %s, width: %d, height: %d", path, width, height);
+    fseek(file, dataOffset, SEEK_SET);
+    for (int y = height - 1; y >= 0; y--) {
+        for (int x = 0; x < width; x++) {
+            uint32_t color;
+            fread(&color, 1, 3, file);
+            pixels[2 + x + y * width] = lilka::display.color565((color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF);
+        }
+    }
+    // Append pixels to images table in registry
+    lua_getfield(L, LUA_REGISTRYINDEX, "images");
+    lua_pushlightuserdata(L, pixels);
+    lua_setfield(L, -2, path);
+    lua_pop(L, 1);
+
+    fclose(file);
+
+    // Return pointer to pixels
+    lua_pushlightuserdata(L, pixels);
+
+    return 1;
+}
+
+static const luaL_Reg lualilka_resources[] = {
+    {"load_image", lualilka_resources_loadImage},
+    {NULL, NULL},
+};
+
+int luaopen_lilka_resources(lua_State* L) {
+    luaL_newlib(L, lualilka_resources);
+    return 1;
+}
+
 int lua_run(String path) {
-    lilka::serial_log("Init Lua libs");
+    lilka::serial_log("Lua: init libs");
 
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
-    lilka::serial_log("Init Lua libs: display");
+    lilka::serial_log("Lua: init display");
     luaL_requiref(L, "display", luaopen_lilka_display, 1);
     lua_pop(L, 1);
-    lilka::serial_log("Init Lua libs: console");
+    lilka::serial_log("Lua: init console");
     luaL_requiref(L, "console", luaopen_lilka_console, 1);
     lua_pop(L, 1);
-    lilka::serial_log("Init Lua libs: controller");
+    lilka::serial_log("Lua: init controller");
     luaL_requiref(L, "controller", luaopen_lilka_controller, 1);
     lua_pop(L, 1);
-    lilka::serial_log("Init Lua libs: util");
+    lilka::serial_log("Lua: init util");
     luaL_requiref(L, "util", luaopen_lilka_util, 1);
     lua_pop(L, 1);
+    lilka::serial_log("Lua: init resources");
+    luaL_requiref(L, "resources", luaopen_lilka_resources, 1);
+    lua_pop(L, 1);
 
-    lilka::serial_log("Init canvas");
+    // Get dir name from path (without the trailing slash)
+    String dir = path.substring(0, path.lastIndexOf('/'));
+    lilka::serial_log("Lua: script dir: %s", dir.c_str());
+    // Store dir in registry with "dir" key
+    lua_pushstring(L, dir.c_str());
+    lua_setfield(L, LUA_REGISTRYINDEX, "dir");
+
+    lilka::serial_log("Lua: init canvas");
     lilka::Canvas canvas;
     lilka::display.setFont(u8g2_font_10x20_t_cyrillic);
     canvas.setFont(u8g2_font_10x20_t_cyrillic);
     canvas.begin();
     // Store canvas in registry with "canvas" key
-    lilka::serial_log("Store canvas in registry");
+    lilka::serial_log("Lua: store canvas in registry");
     lua_pushlightuserdata(L, &canvas);
     lua_setfield(L, LUA_REGISTRYINDEX, "canvas");
+    // Initialize table for image pointers
+    lilka::serial_log("Lua: init memory for images");
+    lua_newtable(L);
+    lua_setfield(L, LUA_REGISTRYINDEX, "images");
 
-    lilka::serial_log("Run lua script");
+    lilka::serial_log("Lua: run script");
     int retCode = luaL_dofile(L, path.c_str());
     if (retCode) {
         const char* err = lua_tostring(L, -1);
         lilka::ui_alert("Lua", String("Помилка: ") + err);
+    }
+
+    lilka::serial_log("Lua: cleanup");
+
+    // Free images from registry
+    lua_getfield(L, LUA_REGISTRYINDEX, "images");
+    lua_pushnil(L);
+    while (lua_next(L, -2) != 0) {
+        // TODO: Is this correct?
+        uint16_t* pixels = (uint16_t*)lua_touserdata(L, -1);
+        delete[] pixels;
+        lua_pop(L, 1);
     }
 
     lua_close(L);
