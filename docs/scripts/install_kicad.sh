7ca47638cc5e71eb670c4cf1f5d48cfbf9ac4be6
Andrew Dunai <a@dun.ai>
2024-02-29 01:19:02 +0200

    docs: revert broken stuff

--- docs/scripts/install_kicad.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-OPTS='-o "Dir::Etc::sourcelist=docs/sources.list" -o "debug::nolocking=true" -o "dir::cache=apt/cache" -o "dir::state=apt/state"'
-apt $OPTS update
-apt $OPTS install kicad
