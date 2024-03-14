a6492a50da976e6509d5e9c2710600e52e400e8d
Andrew Dunai <a@dun.ai>
2024-03-14 17:33:11 +0200

    keira: fix lines demo

--- firmware/keira/src/apps/demos/lines.cpp
+++ firmware/keira/src/apps/demos/lines.cpp
@@ -6,13 +6,12 @@ DemoLines::DemoLines() : App("Лінії") {
 }
 
 void DemoLines::run() {
+    // Створюємо додатковий буфер для малювання ліній.
     lilka::Canvas buffer(canvas->width(), canvas->height());
-    buffer.begin();
     buffer.fillScreen(0);
-    canvas->drawCanvas(&buffer);
-    queueDraw();
 
     while (1) {
+        // Малюємо 64 випадкові лінії на буфері.
         for (int i = 0; i < 64; i++) {
             int x1 = random(0, canvas->width());
             int y1 = random(0, canvas->height());
@@ -21,12 +20,16 @@ void DemoLines::run() {
             uint16_t color = random(0, 0xFFFF);
             buffer.drawLine(x1, y1, x2, y2, color);
         }
+        // Малюємо наш буфер в передній буфер.
         canvas->drawCanvas(&buffer);
         queueDraw();
+
+        // Перевіряємо, чи натиснута кнопка "A".
         if (lilka::controller.getState().a.justPressed) {
-            stop();
             return;
         }
+
+        // Чекаємо 30 мс.
         vTaskDelay(30 / portTICK_PERIOD_MS);
     }
 }
