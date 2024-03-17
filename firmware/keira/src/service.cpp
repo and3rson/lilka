8e8ff9a5fea168262c86de9a215ec83d3aede197
Andrew Dunai <a@dun.ai>
2024-03-17 23:37:01 +0200

    keira: add setStackSize to App & Service keira: reduce visibility of App & Service members

--- firmware/keira/src/service.cpp
+++ firmware/keira/src/service.cpp
@@ -1,6 +1,6 @@
 #include "service.h"
 
-Service::Service(const char* name) : name(name), taskHandle(NULL) {
+Service::Service(const char* name) : name(name), taskHandle(NULL), stackSize(16384) {
 }
 
 Service::~Service() {
@@ -8,11 +8,15 @@ Service::~Service() {
 
 void Service::start() {
     Serial.println("Starting service " + String(name));
-    xTaskCreate(_run, name, 16384, this, 1, &taskHandle);
+    xTaskCreate(_run, name, stackSize, this, 1, &taskHandle);
 }
 
 void Service::_run(void* arg) {
     Service* service = static_cast<Service*>(arg);
     service->run();
     Serial.println("Service " + String(service->name) + " died");
+}
+
+void Service::setStackSize(uint32_t stackSize) {
+    this->stackSize = stackSize;
 }
--- firmware/keira/src/service.h
+++ firmware/keira/src/service.h
@@ -1,17 +1,24 @@
 #pragma once
 
 #include <lilka.h>
+#include "appmanager.h"
 
 class Service {
+    friend class AppManager;
+
 public:
     Service(const char* name);
     virtual ~Service();
     void start();
-    static void _run(void* arg);
+
+protected:
+    void setStackSize(uint32_t stackSize);
 
 private:
+    static void _run(void* arg);
     virtual void run() = 0;
 
     const char* name;
     xTaskHandle taskHandle;
+    uint32_t stackSize;
 };
