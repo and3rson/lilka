9020c3f848ae83dbf7c798de0604d8d54d5ed9f6
Yevhen Boichuk <evgen.b@techspark.engineering>
2024-03-19 16:22:02 +0200

    fix: fix clang format

--- firmware/keira/src/apps/lua/lualilka_wifi.cpp
+++ firmware/keira/src/apps/lua/lualilka_wifi.cpp
@@ -6,8 +6,7 @@
 #include "lualilka_wifi.h"
 #include <lilka.h>
 
-
-int lualilka_wifi_connect(lua_State* L){
+int lualilka_wifi_connect(lua_State* L) {
     int n = lua_gettop(L);
 
     if (n != 2) {
@@ -21,32 +20,31 @@ int lualilka_wifi_connect(lua_State* L){
     return 0;
 }
 
-int lualilka_wifi_status(lua_State* L){
+int lualilka_wifi_status(lua_State* L) {
     lua_pushinteger(L, WiFi.status());
     return 1;
 }
 
-int lualilka_wifi_disconnect(lua_State* L){
+int lualilka_wifi_disconnect(lua_State* L) {
     WiFi.disconnect();
     return 0;
 }
 
 int lualilka_controller_scanNetworks(lua_State* L) {
-
     int n = WiFi.scanNetworks();
 
     lua_createtable(L, n, 0);
     for (int i = 0; i < n; ++i) {
         lua_pushstring(L, WiFi.SSID(i).c_str());
-        lua_rawseti (L, -2, i+1); /* In lua indices start at 1 */
+        lua_rawseti(L, -2, i + 1); /* In lua indices start at 1 */
     }
 
     return 1;
 }
 
-int lualilka_controller_getRSSI(lua_State* L){
+int lualilka_controller_getRSSI(lua_State* L) {
     int n = lua_gettop(L);
-        if (n != 1) {
+    if (n != 1) {
         return luaL_error(L, "Очікується 1 аргументи, отримано %d", n);
     }
 
@@ -57,9 +55,9 @@ int lualilka_controller_getRSSI(lua_State* L){
     return 1;
 }
 
-int lualilka_controller_getEncryptionType(lua_State* L){
+int lualilka_controller_getEncryptionType(lua_State* L) {
     int n = lua_gettop(L);
-        if (n != 1) {
+    if (n != 1) {
         return luaL_error(L, "Очікується 1 аргументи, отримано %d", n);
     }
 
@@ -68,18 +66,17 @@ int lualilka_controller_getEncryptionType(lua_State* L){
     lua_pushinteger(L, WiFi.encryptionType(wifiAccessPoint));
 
     return 1;
-} 
+}
 
-int lualilka_controller_getMAC(lua_State* L){
+int lualilka_controller_getMAC(lua_State* L) {
     byte mac[6];
 
     WiFi.macAddress(mac);
 
     String s;
-    for (byte i = 0; i < sizeof(mac); i++)
-    {
+    for (byte i = 0; i < sizeof(mac); i++) {
         char buf[3];
-        sprintf(buf, "%02X", mac[i]); // J-M-L: slight modification, added the 0 in the format for padding 
+        sprintf(buf, "%02X", mac[i]); // J-M-L: slight modification, added the 0 in the format for padding
         s += buf;
         if (i < 5) s += ':';
     }
@@ -87,9 +84,9 @@ int lualilka_controller_getMAC(lua_State* L){
     lua_pushstring(L, s.c_str());
 
     return 1;
-} 
+}
 
-int lualilka_controller_getIP(lua_State* L){
+int lualilka_controller_getIP(lua_State* L) {
     byte mac[4];
 
     IPAddress ip = WiFi.localIP();
@@ -97,11 +94,11 @@ int lualilka_controller_getIP(lua_State* L){
     lua_pushstring(L, ip.toString().c_str());
 
     return 1;
-} 
+}
 
-int lualilka_controller_config(lua_State* L){
+int lualilka_controller_config(lua_State* L) {
     int n = lua_gettop(L);
-    if (n != 5 ) {
+    if (n != 5) {
         return luaL_error(L, "Очікується 5 аргументів, отримано %d", n);
     }
     IPAddress ip, gateway, subnet, dns1, dns2;
@@ -113,7 +110,7 @@ int lualilka_controller_config(lua_State* L){
     dns2.fromString(lua_tostring(L, 5));
 
     WiFi.config(ip, gateway, subnet, dns1, dns2);
-    
+
     return 0;
 }
 
