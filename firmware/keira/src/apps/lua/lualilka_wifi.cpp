6c1ae8fe5c58a5c686921d593c72ec7ef14c6023
Yevhen Boichuk <evgen.b@techspark.engineering>
2024-03-19 19:44:16 +0200

    fix: fix names -_-

--- firmware/keira/src/apps/lua/lualilka_wifi.cpp
+++ firmware/keira/src/apps/lua/lualilka_wifi.cpp
@@ -30,7 +30,7 @@ int lualilka_wifi_disconnect(lua_State* L) {
     return 0;
 }
 
-int lualilka_controller_scanNetworks(lua_State* L) {
+int lualilka_wifi_scanNetworks(lua_State* L) {
     int n = WiFi.scanNetworks();
 
     lua_createtable(L, n, 0);
@@ -42,7 +42,7 @@ int lualilka_controller_scanNetworks(lua_State* L) {
     return 1;
 }
 
-int lualilka_controller_getRSSI(lua_State* L) {
+int lualilka_wifi_getRSSI(lua_State* L) {
     int n = lua_gettop(L);
     if (n != 1) {
         return luaL_error(L, "Очікується 1 аргументи, отримано %d", n);
@@ -55,7 +55,7 @@ int lualilka_controller_getRSSI(lua_State* L) {
     return 1;
 }
 
-int lualilka_controller_getEncryptionType(lua_State* L) {
+int lualilka_wifi_getEncryptionType(lua_State* L) {
     int n = lua_gettop(L);
     if (n != 1) {
         return luaL_error(L, "Очікується 1 аргументи, отримано %d", n);
@@ -68,7 +68,7 @@ int lualilka_controller_getEncryptionType(lua_State* L) {
     return 1;
 }
 
-int lualilka_controller_getMAC(lua_State* L) {
+int lualilka_wifi_getMAC(lua_State* L) {
     byte mac[6];
 
     WiFi.macAddress(mac);
@@ -86,7 +86,7 @@ int lualilka_controller_getMAC(lua_State* L) {
     return 1;
 }
 
-int lualilka_controller_getIP(lua_State* L) {
+int lualilka_wifi_getIP(lua_State* L) {
     byte mac[4];
 
     IPAddress ip = WiFi.localIP();
@@ -96,7 +96,7 @@ int lualilka_controller_getIP(lua_State* L) {
     return 1;
 }
 
-int lualilka_controller_config(lua_State* L) {
+int lualilka_wifi_config(lua_State* L) {
     int n = lua_gettop(L);
     if (n != 5) {
         return luaL_error(L, "Очікується 5 аргументів, отримано %d", n);
@@ -118,12 +118,12 @@ static const luaL_Reg lualilka_wifi[] = {
     {"connect", lualilka_wifi_connect},
     {"status", lualilka_wifi_status},
     {"disconnect", lualilka_wifi_disconnect},
-    {"scan", lualilka_controller_scanNetworks},
-    {"rssi", lualilka_controller_getRSSI},
-    {"encryptionType", lualilka_controller_getEncryptionType}, // TKIP (WPA) = 2 WEP = 5 CCMP (WPA) = 4 NONE = 7 AUTO = 8
-    {"getMAC", lualilka_controller_getMAC},
-    {"getLocalIP", lualilka_controller_getIP},
-    {"config", lualilka_controller_config},
+    {"scan", lualilka_wifi_scanNetworks},
+    {"rssi", lualilka_wifi_getRSSI},
+    {"encryptionType", lualilka_wifi_getEncryptionType}, // TKIP (WPA) = 2 WEP = 5 CCMP (WPA) = 4 NONE = 7 AUTO = 8
+    {"getMAC", lualilka_wifi_getMAC},
+    {"getLocalIP", lualilka_wifi_getIP},
+    {"config", lualilka_wifi_config},
     {NULL, NULL},
 };
 
