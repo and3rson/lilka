6bf41cf09b6824172c8410616f2666839eca82dd
Andrew Dunai <a@dun.ai>
2024-03-28 13:05:36 +0200

    keira: prevent I2S from stealing pins (closes #74 & #75)

--- sdk/lib/lilka/src/lilka/audio.cpp
+++ sdk/lib/lilka/src/lilka/audio.cpp
@@ -14,6 +14,17 @@ void Audio::begin() {
 #if LILKA_VERSION == 1
     serial_err("Audio not supported in this version of lilka. Try to use Buzzer instead");
 #elif LILKA_VERSION == 2
+
+    // Set up I2S pins globally
+    constexpr uint8_t pinCount = 3;
+    uint8_t pins[pinCount] = {LILKA_I2S_BCLK, LILKA_I2S_LRCK, LILKA_I2S_DOUT};
+    uint8_t funcs[pinCount] = {I2S0O_BCK_OUT_IDX, I2S0O_WS_OUT_IDX, I2S0O_SD_OUT_IDX};
+    for (int i = 0; i < pinCount; i++) {
+        gpio_pad_select_gpio(pins[i]);
+        gpio_set_direction((gpio_num_t)pins[i], GPIO_MODE_OUTPUT);
+        gpio_matrix_out(pins[i], funcs[i], false, false);
+    }
+
     I2S.setAllPins(LILKA_I2S_BCLK, LILKA_I2S_LRCK, LILKA_I2S_DOUT, LILKA_I2S_DOUT, -1);
 
     I2S.begin(I2S_PHILIPS_MODE, 22050, 16);
@@ -30,6 +41,7 @@ void ping_task(void* arg) {
     const int16_t* ping = reinterpret_cast<const int16_t*>(ping_raw);
 
     for (int i = 0; i < ping_raw_size / 2; i++) {
+        // TODO: Should use i2s_write & DMA
         I2S.write(ping[i] >> 2);
         I2S.write(ping[i] >> 2);
     }
