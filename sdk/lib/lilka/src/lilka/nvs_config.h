a6dda6e65723cc7200ea946f74e7a408cb8acd99
Yevhen Boichuk <geri08311@gmail.com>
2024-03-28 11:08:45 +0200

    Format with cpp formater

--- sdk/lib/lilka/src/lilka/nvs_config.h
+++ sdk/lib/lilka/src/lilka/nvs_config.h
@@ -3,15 +3,15 @@
 
 #include <Arduino.h>
 
-namespace lilka{
-    
-    // Define your configuration struct
-    struct Configuration {
-        String language = "en";
-    };
+namespace lilka {
 
-    void saveConfiguration(const Configuration& config);
-    void loadConfiguration(Configuration& config);
-    void setDefaultConfiguration();
-}
+// Define your configuration struct
+struct Configuration {
+    String language = "en";
+};
+
+void saveConfiguration(const Configuration& config);
+void loadConfiguration(Configuration& config);
+void setDefaultConfiguration();
+} // namespace lilka
 #endif // CONFIGURATION_H
