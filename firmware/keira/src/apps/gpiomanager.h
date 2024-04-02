b617f5f0acf8ab06d12a22382eb2296fcc6c0f10
Orphey <15167344+frostmorn@users.noreply.github.com>
2024-04-02 15:31:40 +0000

    cppcheck pinData inital

--- firmware/keira/src/apps/gpiomanager.h
+++ firmware/keira/src/apps/gpiomanager.h
@@ -7,7 +7,7 @@ public:
 
 private:
     uint8_t pinNo[PIN_COUNT] = {LILKA_P3, LILKA_P4, LILKA_P5, LILKA_P6, LILKA_P7, LILKA_P8};
-    uint8_t pinData[PIN_COUNT];
+    uint8_t pinData[PIN_COUNT] = {LOW, LOW, LOW, LOW, LOW, LOW};
     uint8_t pinM[PIN_COUNT] = {INPUT, INPUT, INPUT, INPUT, INPUT, INPUT};
     lilka::Menu menu;
     void readPinData();
