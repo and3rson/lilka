0d14e0fdcdc2506699dccc5bd3c93c4c948b9c29
k3kukua <k3kukua@gmail.com>
2024-05-05 16:37:15 +0300

    add new app BLE Gamepad

---
+++ firmware/keira/src/apps/ble_gamepad/defines.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define BLE_GAMEPAD_APP_NAME          "BLE Геймпад"
+#define DEBUG                         false
+#define DEVICE_NAME                   "Lilka BLE Gamepad"
+#define UI_DELAY_MILLIS               68 // 15 fps
+#define CONTROLLER_TIMER_DELAY_MILLIS 34 // 30 fps
+#define LOG_TAG                       "BLE Gamepad"
+#define UI_FPS_WATCHER                "VIEW LOOP"
+#define MAIN_LOOP_FPS_WATCHER         "MAIN LOOP"
+#define ONE_SECOND                    1000
+#define ONE_MINUTE                    60 * ONE_SECOND
+#define BATTERY_LEVEL_UNKNOWN         -1
+#define BATTERY_LEVEL_NIN             0
+#define BATTERY_LEVEL_MAX             100
