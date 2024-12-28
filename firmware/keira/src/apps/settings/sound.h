fa1694a66f32c244369272f059ac6ac5ccc27874
Alex <15167344+frostmorn@users.noreply.github.com>
2024-12-28 11:50:36 +0000

    Reimplementation of sound settings

--- firmware/keira/src/apps/settings/sound.h
+++ firmware/keira/src/apps/settings/sound.h
@@ -1,10 +1,16 @@
 #include <lilka.h>
 #include "app.h"
+#define VOLUME_STEP         5
+#define VOLUME_BUTTON_DELAY 100
 
 class SoundConfigApp : public App {
 public:
     SoundConfigApp();
 
 private:
+    void saveSettings();
+    int volumeLevel = lilka::audio.getVolume();
+    bool startupSound = lilka::audio.getStartupSoundEnabled();
+    bool startupBuzzer = lilka::buzzer.getStartupBuzzerEnabled();
     void run() override;
 };
